d79e7d686ca1daf8c65bec558fe8aa101a4801e2
silentl007 <xtreemsilentl@gmail.com>
2021-03-04 15:53:15 +0100

    get routes for gallery and freelance

--- database/get.js
+++ database/get.js
@@ -1,18 +1,30 @@
-const express = require('express')
+const express = require('express');
+const mongoModel = require('./model')
 const router = express.Router();
 
 // get gallery data
-router.get('/gallery',async (req, res)=>{
+router.get('/gallery', async (req, res) => {
+    try {
+        const dataGallery = await mongoModel.gallery.find();
+        return res.status(200).json(dataGallery);
+    } catch (err) {
+        console.log(err)
+    }
 
 });
 
 // get freelance data
-router.get('/freelance',async (req, res)=>{
-
+router.get('/freelance', async (req, res) => {
+    try {
+        const dataFreelance = await mongoModel.freelancer.find();
+        return res.status(200).json(dataFreelance);
+    } catch (err) {
+        console.log(err)
+    }
 });
 
 // get user data
-router.get('/user/:id',async (req, res)=>{
+router.get('/user/:id', async (req, res) => {
 
 })
 
