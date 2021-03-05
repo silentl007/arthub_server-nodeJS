3d4509d5ab1b636253d2561ce986f603efbaa83d
silentl007 <xtreemsilentl@gmail.com>
2021-03-05 08:09:29 +0100

    logic to extract specific info when galley or freelance data is pulled

--- database/get.js
+++ database/get.js
@@ -5,8 +5,20 @@ const router = express.Router();
 // get gallery data
 router.get('/gallery', async (req, res) => {
     try {
+        var sendData = [];
         const dataGallery = await mongoModel.gallery.find();
-        return res.status(200).json(dataGallery);
+        if (dataGallery != null) {
+            // logic to loop through to extract specific information needed
+            for (var i = 0; i < dataGallery.length; i++) {
+                var mapData = {
+                    name: dataGallery[i].name, address: dataGallery[i].address,
+                    location: dataGallery[i].location, number: dataGallery[i].number,
+                    works: dataGallery[i].works
+                };
+                sendData.push(mapData);
+            }
+        }
+        return res.status(200).json(sendData);
     } catch (err) {
         console.log(err)
     }
@@ -16,8 +28,20 @@ router.get('/gallery', async (req, res) => {
 // get freelance data
 router.get('/freelance', async (req, res) => {
     try {
+        var sendData = [];
         const dataFreelance = await mongoModel.freelancer.find();
-        return res.status(200).json(dataFreelance);
+        if (dataFreelance != null) {
+            // logic to loop through to extract specific information needed
+            for (var i = 0; i < dataFreelance.length; i++) {
+                var mapData = {
+                    name: dataFreelance[i].name, address: dataFreelance[i].address,
+                    location: dataFreelance[i].location, number: dataFreelance[i].number,
+                    works: dataFreelance[i].works
+                };
+                sendData.push(mapData);
+            }
+        }
+        return res.status(200).json(sendData);
     } catch (err) {
         console.log(err)
     }
