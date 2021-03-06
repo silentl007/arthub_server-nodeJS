e0c76b215ccf345d586a5863fd2c9e39b3ec01bb
silentl007 <xtreemsilentl@gmail.com>
2021-03-06 09:41:03 +0100

    remove item from cart, remove uploaded works

--- database/remove.js
+++ database/remove.js
@@ -2,12 +2,65 @@ const express = require('express');
 const mongoModel = require('./model')
 const router = express.Router();
 
-// remove from cart
-router.delete('/cartremove', async (req, res)=>{
+// remove item from cart all users
+router.delete('/cartremove', async (req, res) => {
     const productID = req.body.productID;
     const userID = req.body.userID;
     const accountType = req.body.accountType;
-    
+    if (accountType == 'Gallery') {
+        try {
+            const query = await mongoModel.gallery.updateOne({ userID: userID },
+                { $pull: { cart: { productID: productID } } });
+            return res.status(200).json(query)
+        } catch (error) {
+            console.log(error);
+            return res.status(400).json({ message: 'Error occurred, check console' })
+        }
+    } else if (accountType == 'Freelancer') {
+        try {
+            const query = await mongoModel.freelancer.updateOne({ userID: userID },
+                { $pull: { cart: { productID: productID } } });
+            return res.status(200).json(query)
+        } catch (error) {
+            console.log(error);
+            return res.status(400).json({ message: 'Error occurred, check console' })
+        }
+    } else {
+        try {
+            const query = await mongoModel.customer.updateOne({ userID: userID },
+                { $pull: { cart: { productID: productID } } });
+            return res.status(200).json(query)
+        } catch (error) {
+            console.log(error);
+            return res.status(400).json({ message: 'Error occurred, check console' })
+        }
+    }
+})
+
+// remove uploaded item for gallery and freelancer
+router.delete('/uploadremove', async (req, res) => {
+    const productID = req.body.productID;
+    const userID = req.body.userID;
+    const accountType = req.body.accountType;
+    if (accountType == 'Gallery') {
+        try {
+            const query = await mongoModel.gallery.updateOne({ userID: userID },
+                { $pull: { works: { productID: productID } } });
+            return res.status(200).json(query)
+        } catch (error) {
+            console.log(error);
+            return res.status(400).json({ message: 'Error occurred, check console' })
+        }
+    } else {
+        try {
+            const query = await mongoModel.freelancer.updateOne({ userID: userID },
+                { $pull: { works: { productID: productID } } });
+            return res.status(200).json(query)
+        } catch (error) {
+            console.log(error);
+            return res.status(400).json({ message: 'Error occurred, check console' })
+        }
+    }
 })
 
 module.exports = router;
